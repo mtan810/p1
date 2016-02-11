c9ca6a04a1943157f4756e5fa46cbf3962427980
Mason Tan <mtan810@gmail.com>
2016-02-10 19:42:25 -0500

    Add placeholder links for p2, p3, and p4.

--- index.php
+++ index.php
@@ -5,5 +5,18 @@
 </head>
 <body>
     <h1>Mason Tan</h1>
+    
+    <h2>Project 2 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</p>
+    
+    <h2>Project 3 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</p>
+    
+    <h2>Project 4 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</p>
+    
 </body>
 </html>
