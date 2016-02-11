db8288a633d3dfbd9400342f0ac41153e2c270e8
Mason Tan <mtan810@gmail.com>
2016-02-10 21:41:26 -0500

    Add bootstrap css.

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <html>
 <head>
     <title>Mason Tan P1 - DWA16 Portfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
     <img src='images/pic.jpg' alt='Mason Tan'>
